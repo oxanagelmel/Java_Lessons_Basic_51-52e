30e90da31a66a202c94bc265e91f7e46796ca93c
Gelmel Oxana <oxana.gelmel@googlemail.com>
2022-11-29 19:53:37 +0100

    hw 3

--- src/lesson3/game/hero/Gnome.java
+++ src/lesson3/game/hero/Gnome.java
@@ -9,9 +9,6 @@ package lesson3.game.hero;
         4. Создать поле id
         5. Создать еще 5 полей (на ваше усмотрение как они будут
         называться и какой тип иметь)
-        6. Создать класс Constant
-        7. Добавить static константы (вспомнить синтаксис)
-        Gnome power, Gnome charisma, Gnome magic
 */
 public class Gnome {
     int id;
