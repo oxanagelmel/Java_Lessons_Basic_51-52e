c025138ec2c5edd2f59a476ce67aea85aac4f686
Gelmel Oxana <oxana.gelmel@googlemail.com>
2022-12-03 02:02:36 +0100

    add new strings

--- src/lesson4Nov24/StringExample.java
+++ src/lesson4Nov24/StringExample.java
@@ -1,10 +1,11 @@
 package lesson4Nov24;
 
+import java.util.Scanner;
+
 public class StringExample {
     public static void main(String[] args) {
         String st = new String("test"); // создание через конструктор
         String str = "test"; // создание через литерал
-      // String strUpper = str.toUpperCase();
         //Нумерация начинается с нуля! 0 = t, 1 = e, 2 = s, 3 = t -> это элементы
         System.out.println(str);
         System.out.println(str.length());  // возвращает длину строки = 4
@@ -15,8 +16,12 @@ public class StringExample {
         System.out.println(str.indexOf("st"));
         System.out.println(str.substring(2));
         System.out.println(str.substring(1, 3));
-      //  System.out.println(strUpper);
+        System.out.println(str.toUpperCase());
         System.out.println(str.replace('t', 'v'));
+
+        Scanner scanner = new Scanner(System.in);
+        String readVal = scanner.nextLine();
+        System.out.println(readVal);
 
     }
 }
