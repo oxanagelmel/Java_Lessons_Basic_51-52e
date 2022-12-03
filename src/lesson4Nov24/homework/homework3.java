4b4d45b1075e98838c74e1188cdca7e108ef4bf1
Gelmel Oxana <oxana.gelmel@googlemail.com>
2022-12-03 02:46:02 +0100

    change hw 3

--- src/lesson4Nov24/homework/homework3.java
+++ src/lesson4Nov24/homework/homework3.java
@@ -5,40 +5,48 @@ package lesson4Nov24.homework;
 //5 Заменить все символы “o” на “a”.
 //6 Преобразуйте строку к верхнему регистру.
 //7 Преобразуйте строку к нижнему регистру.
-//8 Вырезать строку Lessons c помощью метода String.substring().
-//
-//10 программу для подсчета какой индекс у символа 'L'
-//11 Напишите программу, которая спрашивает имя и считывает его с консоли с помощью Scanner
-//   и после пишет на консоль “Hello …….. ! - вместо многоточий имя
-//12 Напишите программу, которая спрашивает имя и считывает его с консоли с помощью Scanner
-//   и после пишет "Ваше имя начинается с буквы …"
-//13 имеется число 59.976, нужно вывести целое из него и перевести в int (явное преобразование типов)
-//14 перевести число 475 типа int в short
 
 import java.util.Scanner;
 
 public class homework3 {
     public static void main(String[] args) {
-       /* String str = "I love Java Lessons!";
+        String str = "I love Java Lessons!";
         System.out.println(str.charAt(0));
-       // System.out.println(str.contains());
+        System.out.println(str.contains("love"));
         System.out.println(str.replace('o', 'a'));
         System.out.println(str.toUpperCase());
         System.out.println(str.toLowerCase());
-        System.out.println(str.substring(12, 19));
-        System.out.println(str.indexOf('L'));*/
 
+//8 Вырезать строку Lessons c помощью метода String.substring().
+        int indexOfL = str.indexOf('L');
+        int indexOfLastChar = str.indexOf('!');
+        System.out.println(str.substring(indexOfL, indexOfLastChar));
+
+//10 программу для подсчета какой индекс у символа 'L'
+        System.out.println(str.indexOf('L'));
+
+//11 Напишите программу, которая спрашивает имя и считывает его с консоли с помощью Scanner
+//   и после пишет на консоль “Hello …….. ! - вместо многоточий имя
         Scanner scan = new Scanner(System.in);
         System.out.println("Как тебя зовут?");
         String Val = scan.nextLine();
         String name = "Oxana";
         System.out.println("Hello " + name);
 
+//12 Напишите программу, которая спрашивает имя и считывает его с консоли с помощью Scanner
+//   и после пишет "Ваше имя начинается с буквы …"
         Scanner scanner = new Scanner(System.in);
         System.out.println("Как тебя зовут?");
         String readVal = scanner.nextLine();
         String n = "Lena";
         System.out.println("Ваше имя начинаемся с буквы " + n.charAt(0));
+
+//13 имеется число 59.976, нужно вывести целое из него и перевести в int (явное преобразование типов)
+
+//14 перевести число 475 типа int в short
+        int i = 475;
+        short fromInt = (short) i;
+        System.out.println(fromInt);
 
     }
 }
