5313770905c27484937d615af0bdcf3e213dbd41
Gelmel Oxana <oxana.gelmel@googlemail.com>
2022-12-08 12:28:31 +0100

    add hw 6

--- src/lesson6Dez01/Homework2.java
+++ src/lesson6Dez01/Homework2.java
@@ -6,8 +6,24 @@ package lesson6Dez01;
 //   Цельсий х 1,8 + 32 = Фаренгейт     Пример: 10°C x 1,8 + 32 = 50°F
 //5) напишите метод для получения суммы трех чисел
 
+import java.util.Scanner;
+
 public class Homework2 {
     public static void main(String[] args) {
-
+        FarnheitCoverter farnheitCoverter = new FarnheitCoverter();
+        System.out.println(farnheitCoverter.fromCelsius(10));
+        System.out.println(farnheitCoverter.toCelcius(50));
     }
+
+    static int calc3Numb() {
+        Scanner scanner = new Scanner(System.in);
+        int one = scanner.nextInt();
+        int two = scanner.nextInt();
+        int three = scanner.nextInt();
+        return one + two + three;
+    }
+    //oder
+ //  static int calc3Numb(int one, int two, int three) {
+ //       return one + two + three;
+ //   }
 }
