59166a42e2a52ea198897564d9a924141040ec89
Gelmel Oxana <oxana.gelmel@googlemail.com>
2022-12-15 10:37:02 +0100

    changed hw 7

--- src/lesson7Dez06/homework/homework7.java
+++ src/lesson7Dez06/homework/homework7.java
@@ -1,28 +1,43 @@
 package lesson7Dez06.homework;
 
 //1) Введите с консоли два числа и выведите больше ли второе число чем первое
-//2) Создайте метод, который принимает 2 числа и возвращает больше или равно первое число
-//   по сравнению со вторым
-//3) создайте метод, который будет принимать 2 строки и писать возвращать строку,
-//   которая составляется склеиванием первой половины первого слова
-//   и второй половины второго слова
-
-// Например:
-// Вход: "привет" и "цветок". На выходе будет "приток".
-// (при - первая половина от первого слова, ток - вторая половина от второго слова)
-
 public class homework7 {
-    public static void main(String[] args) {
+ /*   public static void main(String[] args) {
         int one = 5;
         int two = 9;
         System.out.println("Второе число больше?");
-        System.out.println(two>one); //ответ выводится в boolean
-
-        System.out.println("Первое число больше или равно второму?");
-        System.out.println(one>=two);
-
-        String hello = "привет";
-        String flower = "цветок";
-        System.out.println(hello.substring(0, 3) + flower.substring(3, 6));
+        System.out.println(two > one); //ответ выводится в boolean
     }
+*/
+ //2) Создайте метод, который принимает 2 числа и возвращает больше или равно первое число
+ //   по сравнению со вторым
+    public static void main(String[] args) {
+        boolean res = isMoreOrEquals(4, 5);
+        System.out.println(res);
+    }
+    public static boolean isMoreOrEquals(int first, int second) {
+        return first >= second; //вернуть
+
+        //  System.out.println("Первое число больше или равно второму?");
+        //  System.out.println(one>=two);
+    }
+
+//3) создайте метод, который будет принимать 2 строки и писать возвращать строку,
+//   которая составляется склеиванием первой половины первого слова
+//   и второй половины второго слова
+//   Например:
+//   Вход: "привет" и "цветок". На выходе будет "приток".
+//   (при - первая половина от первого слова, ток - вторая половина от второго слова)
+
+    public static String smartConcat(String str1, String str2){
+        String f = str1.substring(0, str1.length()/ 2);
+        String s = str2.substring(str2.length()/2, str2.length()); // общий случай когда мы вырезаем последнюю строку
+        return f + s;                                              // от середины до конца
+
+
+    }
+
+      //  String str1 = "привет";
+      //  String str2 = "цветок";
+      //  System.out.println(str1.substring(0, 3) + str2.substring(3, 6));
 }
