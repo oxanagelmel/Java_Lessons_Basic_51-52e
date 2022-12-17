8c26392303ae8d8be0ae0cd59d67ddb322752544
Oxana Gelmel <118009410+oxanagelmel@users.noreply.github.com>
2022-12-17 13:41:19 +0100

    Delete Task4.java

--- src/lesson8Dez08/homework/Task4.java
+++
@@ -1,8 +0,0 @@
-package lesson8Dez08.homework;
-
-//4) напишите программу, которая говорит выпал орел или решка
-//   true - орел
-//   false -решка
-//   используйте Random и метод nextBoolean,
-public class Task4 {
-}
